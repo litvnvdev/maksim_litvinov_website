44808f7d04c6af072b60caa6410db6101d2bbbbb
Maxim <maximlitvinov.2611@gmail.com>
2024-03-06 11:54:25 +0300

    create navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
-import { useState } from 'react'
+import { useState } from "react";
+import { ThemeProvider } from "styled-components";
+import { FaMoon } from "react-icons/fa";
+
+import { Nav } from "./components";
+import { GlobalStyles } from "./components/GlobalStyles";
+import { lightTheme, darkTheme } from "./components/Theme";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [theme, setTheme] = useState("light");
+  const handleTheme = () => {
+    theme === "light" ? setTheme("dark") : setTheme("light");
+  };
 
   return (
-    <>
-      <h1>Hi there!</h1>
-    </>
-  )
+    <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
+      <GlobalStyles />
+      <>
+        <Nav>
+          <FaMoon className="theme-icon" onClick={handleTheme} />
+        </Nav>
+      </>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Theme.ts
@@ -0,0 +1,11 @@
+export const lightTheme = {
+    body: '#fff',
+    text: '#2d2e32',
+    background: '#f9f9f9',
+}
+export const darkTheme = {
+    body: '#363537',
+    text: '#FAFAFA',
+    toggleBorder: '#6B8096',
+    background: '#2d2e32',
+}
